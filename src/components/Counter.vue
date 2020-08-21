7a5300dee9a694054806934766f98f2b8f6cc9fb
sakon2856 <sakonwantaeng2856@gmail.com>
2020-08-21 16:36:31 +0700

    master

---
+++ src/components/Counter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div id="example-1">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+      fsize: 50,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    upFunction: function () {
+      this.counter += 1;
+      this.fsize += 10;
+
+      console.log(this.fsize);
+      console.log(this.counter);
+
+      this.$emit("cc", this.counter);
+    },
+    downFunction: function () {
+      this.counter -= 1;
+      this.fsize -= 10;
+
+      console.log(this.fsize);
+      console.log(this.counter);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
